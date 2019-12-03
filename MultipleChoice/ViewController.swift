b24d89dde0796ff24a36ebadd768bf73fafc863d
Sawsan Attyani <sattyani@lcs.on.ca>
2019-12-03 15:03:57 -0500

    Initial Commit

---
+++ MultipleChoice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultipleChoice
+//
+//  Created by Attyani, Sawsan on 2019-12-03.
+//  Copyright © 2019 Attyani, Sawsan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
